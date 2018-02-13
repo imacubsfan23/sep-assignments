42083ca7c7dc3f1b9d26eb676d82c0fae43ee908
Tim D <imacubsfan23@gmail.com>
2018-02-13 21:14:58 +0000

    screen finished

--- 01-data-structures/01-introduction-to-data-structures/screen/screen.rb
+++ 01-data-structures/01-introduction-to-data-structures/screen/screen.rb
@@ -6,18 +6,30 @@ class Screen
   attr_accessor :matrix
 
   def initialize(width, height)
+    self.width = width
+    self.height = height
+    self.matrix = []
+    width.times do |i|
+      temp_array = []
+      height.times do |j|
+        temp_array.push(nil)
+      end
+      self.matrix.push(temp_array)
+    end
   end
-
-  # Insert a Pixel at x, y
+  
   def insert(pixel, x, y)
+    self.matrix[x][y] = pixel if inbounds(x, y)
   end
 
   def at(x, y)
+    self.matrix[x][y] ? self.matrix[x][y] : nil
   end
 
   private
 
   def inbounds(x, y)
+    x >= 0 && x < self.width && y >= 0 && y < self.height
   end
 
 end
