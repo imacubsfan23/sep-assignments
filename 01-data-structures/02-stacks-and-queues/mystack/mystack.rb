d5b601eb028440f1065d5dc6f9e0cd5fa4ff5246
Tim D <imacubsfan23@gmail.com>
2018-02-15 02:04:19 +0000

    finished stack and questions

--- 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
+++ 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
@@ -7,11 +7,18 @@ class MyStack
   end
 
   def push(item)
+    @stack += [item]
+    self.top = item
   end
 
   def pop
+    temp = self.top
+    @stack = @stack[0...@stack.length - 1]
+    self.top = @stack.last
+    temp
   end
 
   def empty?
+    @stack.length == 0
   end
 end
