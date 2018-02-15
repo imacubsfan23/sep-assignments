90b297e4491d35bf788b841c285a8d0abd080185
Tim D <imacubsfan23@gmail.com>
2018-02-15 01:39:29 +0000

    queue works

--- 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
+++ 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
@@ -5,14 +5,23 @@ class MyQueue
   def initialize
     @queue = Array.new
     @head = @queue[0]
+    @tail = @queue.last
   end
 
   def enqueue(element)
+    @queue += [element]
+    @head = @queue.first
+    @tail = element
   end
 
   def dequeue
+    temp = @head
+    @queue = @queue[1...@queue.length]
+    @head = @queue.first
+    temp
   end
 
   def empty?
+    @queue.length == 0
   end
 end
