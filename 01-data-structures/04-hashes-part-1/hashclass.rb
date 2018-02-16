0e0ba33486943af6290438411e9fc22f0a86efc9
Tim D <imacubsfan23@gmail.com>
2018-02-16 19:14:05 +0000

    hashes part 1 and questions completed

--- 01-data-structures/04-hashes-part-1/hashclass.rb
+++ 01-data-structures/04-hashes-part-1/hashclass.rb
@@ -1,3 +1,5 @@
+require_relative 'hash_item'
+
 class HashClass
 
   def initialize(size)
@@ -5,23 +7,51 @@ class HashClass
   end
 
   def []=(key, value)
+    i = index(key, size)
+    if @items[i] == nil
+      @items[i] = HashItem.new(key, value)
+    elsif @items[i].key == key && @items[i].value == value
+      "duplicate"
+    else
+      # resize until collision is fixed
+      while true do
+        resize
+        j = index(key, size)
+        break if @items[j] == nil || @items[j].key == key
+      end
+      j = index(key, size)
+      if @items[j] == nil
+        @items[j] = HashItem.new(key, value)
+      else
+        @items[j].value = value
+      end
+    end
   end
 
 
   def [](key)
+    i = index(key, size)
+    @items[i].value if @items[i]
   end
 
   def resize
+    arr = Array.new(@items.length * 2)
+    (0...@items.length).each do |i|
+      if @items[i]
+        index = @items[i].key.sum % arr.length
+        arr[index] = @items[i]
+        @items[i] = nil
+      end
+    end
+    @items = arr
   end
 
-  # Returns a unique, deterministically reproducible index into an array
-  # We are hashing based on strings, let's use the ascii value of each string as
-  # a starting point.
   def index(key, size)
+    key.sum % size
   end
 
-  # Simple method to return the number of items in the hash
   def size
+    @items.length
   end
 
 end
