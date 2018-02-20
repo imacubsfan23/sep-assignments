acb58031a694af539839c90aeb562f74506de999
Tim D <imacubsfan23@gmail.com>
2018-02-20 21:40:14 +0000

    hashes part 2 completed

--- 01-data-structures/05-hashes-part-2/open_addressing/open_addressing.rb
+++ 01-data-structures/05-hashes-part-2/open_addressing/open_addressing.rb
@@ -2,29 +2,77 @@ require_relative 'node'
 
 class OpenAddressing
   def initialize(size)
+    @hash = Array.new(size)
+    @count = 0
   end
 
   def []=(key, value)
+    i = index(key, size)
+    if !@hash[i]
+      @hash[i] = Node.new(key, value)
+      @count += 1
+    elsif @hash[i].key == key && @hash[i].value == value
+      return
+    else
+      next_index = next_open_index(i)
+      if @hash[i].key == key && @hash[i].value != value && next_index == -1
+        resize
+        @hash[next_index].value = value
+        @count += 1
+      elsif next_index == -1
+        resize
+        self[key] = value
+      else
+        @hash[next_index] = Node.new(key, value)
+        @hash[next_index].value = value
+        @count += 1
+      end
+    end
   end
 
   def [](key)
+    i = index(key, size)
+    while i < size
+      if @hash[i].key == key
+        return @hash[i].value
+      else
+        i += 1
+      end
+    end
+  end
+  
+  def print
+    (0...size()).each do |i|
+      if @hash[i] != nil
+        puts "Index #{i}: [#{@hash[i].key}] = #{@hash[i].value}"
+      end
+    end
+    puts "@count = #{@count}"
   end
 
-  # Returns a unique, deterministically reproducible index into an array
-  # We are hashing based on strings, let's use the ascii value of each string as
-  # a starting point.
   def index(key, size)
+    key.sum % size
   end
 
-  # Given an index, find the next open index in @items
   def next_open_index(index)
+    while @hash[index]
+      index += 1
+    end
+    index >= size ? -1 : index
   end
 
-  # Simple method to return the number of items in the hash
   def size
+    @hash.length
   end
 
-  # Resize the hash
   def resize
+    temp = @hash
+    @hash = Array.new(size * 2)
+    temp.each do |i|
+      if i
+        index = index(i.key, @hash.length)
+        @hash[index] = Node.new(i.key, i.value)
+      end
+    end
   end
 end
