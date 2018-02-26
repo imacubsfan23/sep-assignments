8a8d8b8c067b1991dc4682e388c04d90094d5d62
Tim D <imacubsfan23@gmail.com>
2018-02-26 15:55:34 +0000

    hashes 2, forgot to commit

--- 01-data-structures/05-hashes-part-2/separate_chaining/linked_list.rb
+++ 01-data-structures/05-hashes-part-2/separate_chaining/linked_list.rb
@@ -4,27 +4,60 @@ class LinkedList
   attr_accessor :head
   attr_accessor :tail
 
-  # This method creates a new `Node` using `data`, and inserts it at the end of the list.
   def add_to_tail(node)
+    self.head = node if self.head == nil
+    self.tail.next = node if self.tail
+    self.tail = node
   end
 
-  # This method removes the last node in the lists and must keep the rest of the list intact.
   def remove_tail
+    return if self.tail == nil
+    if self.head == self.tail
+      self.head = nil
+      self.tail = nil
+    else
+      temp = self.head
+      while temp && temp.next != self.tail
+        temp = temp.next
+      end
+      temp.next = nil
+      self.tail = temp
+    end
   end
 
-  # This method prints out a representation of the list.
   def print
+    str = ""
+    temp = self.head
+    while temp
+      str +=  "#{temp.data}\n"
+      temp = temp.next
+    end
+    puts str
   end
 
-  # This method removes `node` from the list and must keep the rest of the list intact.
   def delete(node)
+    if node == self.head
+      self.head = self.head.next
+    elsif node == self.tail
+      remove_tail
+    else
+      temp = self.head
+      while temp && temp.next != node
+        temp = temp.next
+      end
+      temp.next = node.next
+    end
   end
 
-  # This method adds `node` to the front of the list and must set the list's head to `node`.
   def add_to_front(node)
+    temp = self.head
+    self.head = node
+    self.head.next = temp
   end
 
-  # This method removes and returns the first node in the Linked List and must set Linked List's head to the second node.
   def remove_front
+    temp = self.head
+    self.head = self.head.next
+    temp
   end
 end
--- 01-data-structures/05-hashes-part-2/separate_chaining/separate_chaining.rb
+++ 01-data-structures/05-hashes-part-2/separate_chaining/separate_chaining.rb
@@ -5,29 +5,79 @@ class SeparateChaining
 
   def initialize(size)
     @max_load_factor = 0.7
+    @count = 0
+    @hash = Array.new(size)
   end
 
   def []=(key, value)
+    i = index(key, size)
+    @hash[i] = LinkedList.new if @hash[i] == nil
+    @hash[i].add_to_tail(Node.new(key, value))
+    @count += 1
+    self.resize if load_factor >= @max_load_factor
   end
 
   def [](key)
+    i = index(key, size)
+    if @hash[i]
+      node = @hash[i].head
+      while node
+        return node.value if node.key == key
+        node = node.next
+      end
+    end
   end
-
-  # Returns a unique, deterministically reproducible index into an array
-  # We are hashing based on strings, let's use the ascii value of each string as
-  # a starting point.
+  
+  def print
+    @hash.each do |list|
+      if list
+        node = list.head
+        while node
+          puts "[#{node.key}] = #{node.value}"
+          node = node.next
+        end
+      end
+    end
+    "Load Factor: #{load_factor}, Count: #{@count}"
+  end
+  
   def index(key, size)
+    key.sum % size
   end
 
-  # Calculate the current load factor
   def load_factor
+    @count / size.to_f
   end
 
-  # Simple method to return the number of items in the hash
   def size
+    @hash.length
   end
 
-  # Resize the hash
+=begin
+  def next_prime(n)
+    primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199]
+    i = 0
+    while primes[i] < n
+      i += 1
+    end
+    primes[i + 1]
+  end
+=end
+
   def resize
+    temp = @hash
+    @hash = Array.new(size * 2)
+    temp.each do |list|
+      if list
+        node = list.head
+        while node
+          i = index(node.key, size)
+          @hash[i] = LinkedList.new if !@hash[i]
+          @hash[i].add_to_tail(Node.new(node.key, node.value))
+          node = node.next
+        end
+      end
+    end
+    resize if load_factor >= @max_load_factor
   end
 end
