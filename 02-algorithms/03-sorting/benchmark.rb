3ea34500b80619fc8a397ced7d9fb5f46187d34a
Tim D <imacubsfan23@gmail.com>
2018-03-19 17:47:18 +0000

    fixed duplication issue in benchmark

--- 02-algorithms/03-sorting/benchmark.rb
+++ 02-algorithms/03-sorting/benchmark.rb
@@ -7,15 +7,15 @@ array = (1..50).to_a.shuffle!
 
 Benchmark.bm(10) do |x|
     x.report("bucket sort") do
-        bucket_array = array
+        bucket_array = array.dup
         bucket_sort(bucket_array)
     end
     x.report("heap sort") do
-        heap_array = array
+        heap_array = array.dup
         heap_sort(heap_array)
     end
     x.report("quick sort") do
-        quick_array = array
+        quick_array = array.dup
         quick_sort(quick_array, 0, 50)
     end
 end
